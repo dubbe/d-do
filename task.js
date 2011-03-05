b3afe70ab934ac5b58d0cb171d52e61a247aaa15
Thomas <thomas@dhlbrg.se>
2011-03-05 15:16:25 +0100

    render tasks now returns all tasks. added a view when a task is created.

--- task.js
+++ task.js
@@ -6,25 +6,52 @@ Task = function(host, port) {
         raw: false
     });
     this.db = this.connection.database('d-do') ;
+    this.db.create() ;
 } ;
 
 Task.prototype.create = function(task, callback) {
+   
+    this.db.save('_design/tasks', {
+        all: {
+            map: function (doc) {
+                if (doc.type == 'task') {
+                    emit(null, doc);
+                }
+            }
+        }
+    });
+    
     this.db.save(task, function(error, result) {
-      if( error ) callback(error)
-      else callback(null, task);
+      if (error) {
+          callback(error)
+      }
+      else {
+          callback(null, task);
+      }
+          
     });
 }
 
 Task.prototype.render = function(data, callback) {
-    callback(null, data) ;
+    
+    this.db.view('tasks/all',function(error, result) {
+        if( error ){
+            callback(error)
+        }else{
+            var docs = [];
+            result.forEach(function (row){
+                docs.push(row);
+            });
+            callback(null, docs);
+        } 
+    });
+    
 }
 Task.prototype.update = function(task, data, callback) {
 }
 Task.prototype.assign = function(task, data, callback) {
 }
 Task.prototype.updateTimeLeft = function(task, data, callbak) {
-}
-Task.prototype.getAll = function(callback) {
 }
 
 exports.Task = Task ;
